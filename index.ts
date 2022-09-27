39eb4790073a52369c742fb1eb186742375584a4
dumbcache <dumbcache@gmail.com>
2022-09-27 15:27:39 +0530

    version2: lists files and then directories respectively,recursively

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import fs from "fs/promises";
 import path from "path";
 import chalk, { ChalkInstance } from "chalk";
+import { dir } from "console";
 
 const hLine = "\u2500";
 const vLine = "\u2502";
@@ -20,25 +21,41 @@ const readFilesInternal = async (
     color: ChalkInstance = initColor,
     space: string = ""
 ) => {
+    let dirs: string[] = [],
+        fls: string[] = [];
+
     let padding = initColor(vLine) + space;
     let dirColor = colors[rand()];
     while (dirColor === color) {
         dirColor = colors[rand()];
     }
     const files = await fs.readdir(readFrom);
+
     for (let file of files) {
-        process.stdout.write(padding);
         let filePath = path.resolve(readFrom, file);
         const fileData = await fs.lstat(filePath);
         if (fileData.isDirectory()) {
-            console.log(color(hLine) + " " + dirColor("./" + file));
+            dirs.push(file);
+            continue;
+        }
+        fls.push(file);
+    }
+
+    for (let file of fls) {
+        process.stdout.write(padding);
+        console.log(color(hLine) + " " + file);
+    }
+    for (let dir of dirs) {
+        process.stdout.write(padding);
+        let filePath = path.resolve(readFrom, dir);
+        const fileData = await fs.lstat(filePath);
+        if (fileData.isDirectory()) {
+            console.log(color(hLine) + " " + dirColor("./" + dir));
             await readFilesInternal(
                 filePath,
                 dirColor,
                 space + "  " + dirColor(vLine)
             );
-        } else {
-            console.log(color(hLine) + " " + file);
         }
     }
 };
