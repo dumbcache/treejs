0d073b55cfe76d68117527a142031a2a0a3430f7
dumbcache <dumbcache@gmail.com>
2022-09-29 11:55:57 +0530

    small changes

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import fs from "fs/promises";
 import path from "path";
 import chalk, { ChalkInstance } from "chalk";
-import { dir } from "console";
 
 const hLine = "\u2500";
 const vLine = "\u2502";
@@ -16,35 +15,38 @@ const colors = [
 const rand = () => Math.round(Math.random() * (colors.length - 1));
 const initColor = colors[rand()];
 
-const readFilesInternal = async (
-    readFrom: string,
-    color: ChalkInstance = initColor,
-    space: string = ""
-) => {
-    let dirs: string[] = [],
-        fls: string[] = [];
-
-    let padding = initColor(vLine) + space;
-    let dirColor = colors[rand()];
-    while (dirColor === color) {
-        dirColor = colors[rand()];
-    }
+export const readFiles = async (readFrom: string): Promise<string[][]> => {
     const files = await fs.readdir(readFrom);
-
+    const dirs: string[] = [],
+        fls: string[] = [];
     for (let file of files) {
         let filePath = path.resolve(readFrom, file);
         const fileData = await fs.lstat(filePath);
         if (fileData.isDirectory()) {
             dirs.push(file);
             continue;
+        } else {
+            fls.push(file);
         }
-        fls.push(file);
     }
+    return [fls, dirs];
+};
 
-    for (let file of fls) {
+const logFiles = (files: string[], padding: string, color: ChalkInstance) => {
+    for (let file of files) {
         process.stdout.write(padding);
         console.log(color(hLine) + " " + file);
     }
+};
+
+const logDirs = async (
+    dirs: string[],
+    readFrom: string,
+    padding: string,
+    space: string,
+    color: ChalkInstance,
+    dirColor: ChalkInstance
+) => {
     for (let dir of dirs) {
         process.stdout.write(padding);
         let filePath = path.resolve(readFrom, dir);
@@ -53,17 +55,34 @@ const readFilesInternal = async (
             console.log(color(hLine) + " " + dirColor("./" + dir));
             await readFilesInternal(
                 filePath,
-                dirColor,
-                space + "  " + dirColor(vLine)
+                space + "  " + dirColor(vLine),
+                dirColor
             );
         }
     }
 };
 
-export const readFiles = (readFrom: string) => {
+const readFilesInternal = async (
+    readFrom: string,
+    space: string = "",
+    color: ChalkInstance = initColor
+) => {
+    let padding = initColor(vLine) + space;
+    let dirColor = colors[rand()];
+    while (dirColor === color) {
+        dirColor = colors[rand()];
+    }
+
+    const [files, dirs] = await readFiles(readFrom);
+    logFiles(files, padding, color);
+    await logDirs(dirs, readFrom, padding, space, color, dirColor);
+};
+
+export const fileTree = (readFrom: string) => {
     console.log(initColor(readFrom));
     readFilesInternal(readFrom);
 };
 
 const readFrom = "/home/dumbcache/Desktop";
+fileTree(readFrom);
 readFiles(readFrom);
